9bdebcd6d9151d70ea4fdf782621fe4da7394c17
VaibhavKhushalani <vaibhavkhushlani04@gmail.com>
2024-09-01 14:18:41 +0530

    erros removed

--- src/components/MainComponent.jsx
+++ src/components/MainComponent.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import styled from "styled-components";
 import ShortenDesktop from "../assests/bg-shorten-desktop.svg";
